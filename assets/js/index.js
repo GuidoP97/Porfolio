6a148e9cbc391dbfd8e69a7bb350d764539225cc
GuidoP97 <guidopodesta99@gmail.com>
2021-05-27 17:54:54 -0300

    nuevo icon que se utiliza cuando la burger esta checked, se añadio su logica y diseño.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,16 @@
 const hamburger = document.getElementById("hamburger");
 const checkbox = document.getElementById("check");
 const menu = document.getElementById("navbar__menu");
+const icon = hamburger.children[0];
 
 hamburger.addEventListener("click", () => {
   if (checkbox.hasAttribute("checked")) {
     checkbox.removeAttribute("checked");
     menu.classList.remove("open");
+    icon.setAttribute("src", "assets/images/icon-hamburger.svg");
   } else {
     checkbox.setAttribute("checked", "");
     menu.classList.add("open");
+    icon.setAttribute("src", "assets/images/icon_cross.svg");
   }
 });
